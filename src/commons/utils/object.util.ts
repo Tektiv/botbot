8c909d6d7b869aebb908aad6ee0ca13c8af9e410
Victor <simart.victor@gmail.com>
2022-11-18 23:42:46 +0200

    ✨(ObjectUtils): add get method

--- src/commons/utils/object.util.ts
+++ src/commons/utils/object.util.ts
@@ -1,3 +1,5 @@
+import { NestedPaths, TypeFromPath } from '../types/nested-path';
+
 /**
  * A class of utils to manipulate objects.
  */
@@ -9,5 +11,24 @@ export class ObjectUtils {
    */
   static fromEntries<K extends keyof any, V>(array: Array<[K, V]>): Record<string, V> {
     return array.reduce((obj, [key, value]) => Object.assign(obj, { [key]: value }), {} as Record<K, V>);
+  }
+
+  /**
+   * Gets the value of a nested property of an object based on its path.
+   * @param obj
+   * @param path
+   * @param defaultValue
+   */
+  static get<K extends keyof any, V, Path extends NestedPaths<Record<K, V>>>(
+    obj: Record<K, V>,
+    path?: Path,
+    defaultValue: any = undefined,
+  ): TypeFromPath<Record<K, V>, Path> {
+    if (!path) {
+      return (obj as any) || defaultValue;
+    }
+    return path
+      .split('.')
+      .reduce((subObject: any, subKey: any) => (subObject ? subObject[subKey] : defaultValue), obj) as any;
   }
 }
