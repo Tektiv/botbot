00cde75a6ea59efcb031856e83e2529bb605f666
Clarybou <clary.masson@gmail.com>
2023-07-26 11:20:19 +0200

    chore: 🔧 enable typescript esModuleInterop

--- src/commons/utils/request.util.ts
+++ src/commons/utils/request.util.ts
@@ -1,6 +1,6 @@
 import { from, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import * as needle from 'needle';
+import needle from 'needle';
 
 type RequestOptions = Partial<{
   headers: Record<string, string>;
