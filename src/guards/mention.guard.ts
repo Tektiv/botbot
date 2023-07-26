fbbb609d4e1145f71cd71b603d277d81d1abc85c
Clarybou <clary.masson@gmail.com>
2023-07-26 14:24:02 +0200

    fix(mention.guard): 🐛 fixed trigger for pasted mention

--- src/guards/mention.guard.ts
+++ src/guards/mention.guard.ts
@@ -3,7 +3,7 @@ import { ArgsOf, Discord, GuardFunction } from 'discordx';
 @Discord()
 export class MentionGuard {
   static Botbot: GuardFunction<ArgsOf<'messageCreate'>> = async ([message], client, next) => {
-    if (message.mentions.members?.has(client.user?.id!)) {
+    if (message.mentions.members?.has(client.user?.id!) || message.content.includes('<@&524858039300259850>')) {
       await next();
     }
   };
